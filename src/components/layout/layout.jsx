a820d87366e81743754e868aa2c37be124a718a4
anuj bhatt <anujbhatt023@gmail.com>
2020-08-19 19:30:07 +0530

    added admin tables

--- src/components/layout/layout.jsx
+++ src/components/layout/layout.jsx
@@ -4,6 +4,8 @@ import Header from "./header/header"
 import NavigationDots from "./navigation-dots/navigation-dots"
 import  Map from "./map/map"
 import Admin from "./admin/admin"
+import BelieverList from "./admin/believer-list/believer-list"
+import NotABelieverList from "./admin/not-a-believer-list/not-a-believer-list"
 import {Switch,Route} from "react-router-dom"
 
 document.addEventListener('popstate', function (event) {
@@ -12,6 +14,15 @@ document.addEventListener('popstate', function (event) {
 
  class Layout extends Component{
 
+    state={
+      adminVerified:false
+    }
+
+    verificationHandler=()=>{
+      this.setState({
+        adminVerified:true
+      })
+    }
 
    render(){
 
@@ -27,7 +38,13 @@ document.addEventListener('popstate', function (event) {
                   <Map/>
               </Route>
               <Route exact  path="/admin">
-                  <Admin/>
+                  <Admin verify={this.verificationHandler}/>
+              </Route>
+              <Route exact  path="/admin/believer">
+                  <BelieverList adminVerified={this.state.adminVerified} />
+              </Route>
+              <Route exact  path="/admin/notABeliever">
+                  <NotABelieverList adminVerified={this.state.adminVerified} />
               </Route>
 
            </Switch>
