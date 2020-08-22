0800c1383ece4991fd2ab222aaf6e0c5afe39fd3
anuj bhatt <anujbhatt023@gmail.com>
2020-08-22 18:33:02 +0530

    commit for push

--- src/components/layout/email-check/email-check.jsx
+++ src/components/layout/email-check/email-check.jsx
@@ -9,10 +9,12 @@ import axios from 'axios';
         return (
             <div className="landing__item landing__item--11 landing__item--11-2">
                  <div style={{left:"0"}} className="landing__item-text landing__item-text-1"><i className="quote-left fa fa-quote-left" aria-hidden="true"></i>check you email for verification<i className="quote-left fa fa-quote-right" aria-hidden="true"></i></div>
-                 <button onClick={()=>{window.location=
-                                  // "https://ancient-woodland-30225.herokuapp.com/map"
-                                  "http://localhost:3000/map"
-                             }} className="landing__item-button">Go Back</button>
+                 {
+                 // <button onClick={()=>{window.location=
+                 //                  // "https://ancient-woodland-30225.herokuapp.com/map"
+                 //                  "http://localhost:3000/map"
+                 //             }} className="landing__item-button">Go Back</button>
+                 }
             </div>
         )
     }
