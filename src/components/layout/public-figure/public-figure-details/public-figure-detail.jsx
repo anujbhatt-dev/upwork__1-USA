f748ed23599b2ba8d79b1e064ac4e6ee97d40fc3
anuj bhatt <anujbhatt023@gmail.com>
2020-09-01 21:08:20 +0530

    push

--- src/components/layout/public-figure/public-figure-details/public-figure-detail.jsx
+++ src/components/layout/public-figure/public-figure-details/public-figure-detail.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
+import Header from "../../header/header"
+import Gravatar from 'react-awesome-gravatar';
+import Flag from 'react-world-flags';
 
  class PubliFigureDetail extends Component {
 
@@ -17,11 +20,46 @@ import { withRouter } from 'react-router-dom';
 
 
     render() {
+         console.log(this.state.client);
+         //   size:40,
+         //   default:"identicon"
+         // }} email={this.state.client.email}>
+         //options={{
+         // <Gravatar email={this.state.client.email}
+         // { url => (<img className="gravatar__img" src={url} />) }
+         // </Gravatar>
+        return (<>
+             <Header />
+            <div className = "clientDetailViewWrapper" >
+                <div className = "clientDetailView">
+                  <div className="clientDetailView__row">
+                      <div className = "clientDetailView__col clientDetailView__col-1">
+                          <div className = "clientDetailView__name">{this.state.client.firstName+" "+this.state.client.lastName}</div>
+                          <div className="clientDetailView__notableFor">Notable as <span>{this.state.client.publicFigure==="PF1"?"Public Figure":"Scientist"}</span></div>
+                          <div className="clientDetailView__verified">{this.state.client.verified?<span><i class="fa fa-check" aria-hidden="true"></i> verified</span>:null}</div>
+                      </div>
+                      <div className="clientDetailView__col clientDetailView__col-2">
+                            <div className className="clientDetailView__col-country"><span>{this.state.client.country}</span></div>
+                           <Flag  code={this.state.client.code} height={25}/>
+                      </div>
+                      <div className="clientDetailView__col clientDetailView__col-3">
+                      <Gravatar email="anujbhatt023@gmail.com" options={ {
+                        default:"identicon",
+                        size:120,
+                      } }>
+                            { url => (<img className="gravatar__img clientDetailView__img" src={url} />) }
+                      </Gravatar>
+                      </div>
+                  </div>
 
-        return (
-            <div>
-                {this.state.client.firstName}
+                    <hr  className="hr"/>
+                    <div className="clientDetailView__background">
+                        {this.state.client.background}
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla laudantium tempore repellendus, cupiditate quaerat tenetur error quia doloremque molestias placeat fugit, consectetur rerum sit adipisci aspernatur provident ipsa odit, incidunt itaque. Quod facere, quia, ea, vel quis magnam, harum cum architecto ab aut ipsam? Quia vitae, dignissimos delectus amet nemo officiis id, sequi harum labore sed quaerat atque ullam dicta cumque illo tempora repudiandae illum velit vero numquam? Tenetur, iste placeat pariatur, veniam dolor molestias asperiores aliquam porro nobis vitae nihil eveniet praesentium excepturi sequi nostrum. Rerum doloribus, esse! Tenetur saepe, obcaecati iusto expedita iure minima ullam quos sunt culpa, enim aspernatur iste animi. Repellat pariatur commodi similique error, laboriosam recusandae omnis ratione eius reiciendis quia ipsum, dolores quos perspiciatis sint rerum. Ut culpa quasi minus, praesentium inventore officia ex, facere repellendus quaerat, vel eius laboriosam nesciunt tenetur itaque. Voluptate ratione a ducimus tenetur neque necessitatibus minus dicta voluptas labore ab doloribus excepturi ut, sapiente adipisci facilis repudiandae, non totam asperiores. Illo deleniti culpa nostrum placeat expedita incidunt nam a quos magnam et dolorum voluptates ducimus velit consectetur, similique officiis ipsa iure dignissimos nobis vitae animi consequatur maxime architecto, impedit. Adipisci distinctio nostrum, iure saepe deserunt unde dolorem perspiciatis eius ratione quod reiciendis expedita officiis ex numquam nihil labore explicabo voluptatum tenetur quos maxime aliquid. Nemo explicabo, corporis harum quaerat delectus a recusandae dolorem laudantium mollitia autem sit exercitationem est possimus eum dicta, repudiandae voluptas, temporibus iusto magnam. Sit, necessitatibus, optio. Officiis soluta consectetur perferendis voluptatem, voluptas optio architecto assumenda laborum suscipit earum impedit corrupti distinctio veritatis numquam, praesentium quod quisquam. A eligendi error aut quos, quibusdam dignissimos fugit illum repellat deleniti? Aspernatur labore in neque repellendus tempora officia sit laudantium autem facere dicta iste tempore cumque, tenetur saepe molestias, aut voluptatum impedit minus ad similique! Architecto magnam fugiat harum molestias, pariatur perspiciatis illo consequatur vitae, adipisci excepturi eveniet error laborum rerum laboriosam quo blanditiis deleniti, quos saepe ratione modi soluta, quam explicabo. Repudiandae impedit nesciunt ab laboriosam in at blanditiis cumque amet dicta delectus! Voluptatem amet nesciunt quidem beatae quo id aspernatur quibusdam aliquam obcaecati nulla commodi illum temporibus rerum accusantium quaerat hic illo voluptas in, dolorum! Ad facilis quod dolor. Iusto dolores architecto reprehenderit consequatur, harum deserunt quae incidunt laboriosam. Blanditiis vitae porro, earum. Dicta error harum ducimus suscipit, deserunt, iusto autem optio reiciendis eaque obcaecati esse ipsa quibusdam ut mollitia voluptatibus doloribus, facere sit ipsum ex. Voluptatibus sequi explicabo reprehenderit debitis incidunt dolorem, voluptatem facere quo. Sed, atque dolores, veritatis voluptas itaque porro veniam, id modi sint ullam aut mollitia illum facilis harum! Quod recusandae facere necessitatibus soluta quas illo praesentium tempora in eos tempore, saepe dolore similique, vero eveniet dolor, iste dignissimos labore distinctio quisquam qui repudiandae, delectus laborum quam! Labore enim cupiditate itaque sunt recusandae soluta accusamus rerum totam aliquid, doloremque quod at illum molestiae voluptatibus, reprehenderit illo quos ipsum eveniet doloribus libero? Velit totam eius consectetur porro ullam, quos qui eveniet quae quidem deleniti tempore facilis omnis mollitia temporibus officiis, voluptatibus, excepturi nemo voluptatem.
+                    </div>
+                </div>
             </div>
+            </>
         )
     }
 }
--- src/components/layout/public-figure/public-figure.jsx
+++ src/components/layout/public-figure/public-figure.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import axios from 'axios';
 import Flag from 'react-world-flags';
-import Header from "../header/header"
 import Aos from "aos"
 import "aos/dist/aos.css"
+import Header from "../header/header"
 import Gravatar from 'react-awesome-gravatar';
 import { NavLink } from 'react-router-dom';
 
@@ -68,7 +68,7 @@ axios.get("/v1/client/publicFigure/all/all/0").then(res=>{
         if(this.state.loading)
         axios.get("/v1/client/publicFigure/"+this.state.country+"/"+(this.state.search.length<=0?"all":this.state.search)+"/"+this.state.page).then(res=>{
              this.setState((state)=>{return {data:state.data.concat(res.data.content),loading:false,totalPages:res.data.totalPages}});
-        
+
 })
     }
 
@@ -117,20 +117,41 @@ axios.get("/v1/client/publicFigure/all/all/0").then(res=>{
             <Header/>
             <div className="notables">
 
+            <div className="searchWrapper">
+             <div className="notables__search">
+                 <input className="notables__search-input" type="text" value={this.state.search} onChange={(e)=>this.searchInputHandler(e)}  placeholder="search" name="" id=""/>
+             </div>
+             <div className="notables__searchByCountry">
+             <input className="notables__searchByCountry-input" type="text"  onChange={(e)=>this.searchedCountriesHandler(e)} name="searchedCahrecterForCountry" placeholder="country search" value={this.state.searchedCahrecterForCountry} id=""/>
+             <div className="notables__searchByCountry-dropdown">
+             {this.state.searchedCountries.map(c=>
+                                  <div style={{zIndex:"1000"}} className="notables__searchByCountry-dropdown-item" onClick={()=> this.countrySelectHandler(c.substring(0,c.indexOf(',')))}>
+                                      <Flag code={c.substring(c.indexOf(',')+1)}height={16} />
+                                      {c.slice(0,c.length-3)}
+                                   </div>
+                                  )}
+                     <div className="notables__searchByCountry-dropdown-item" onClick={()=>this.countrySelectHandler("all")}>
+                         ALL
+                     </div>
+                 </div>
+             </div>
+             </div>
+
+
               <div className="searchfor"> <strong>{this.state.country==="all"?null:this.state.country}</strong></div>
                   <div className="userWrapper">
                 {this.state.data.map((d,i)=><>
                   <div   className={"user "+"user"+(i%2)}>
                        <div data-aos={i%2===0?"fade-right":"fade-left"}  className="userWrapper2">
-                           <div className="userWrapper3">
+
 
                 <div className="user__name"><span>{d.firstName+" "+d.lastName}{d.id}</span></div>
                            <div className="user__field user__field1">Natable as <span>{d.publicFigure==="PF1"?"Public Figure":"Scientist"}</span></div>
                            <div className="user__field user__field2">
                               {d.background===null?
-                                null:
+                                <br/>:
                                 <span>{d.background.length<20?
-                                 <span> {d.background.padEnd(d.background.length+5,'.')} <span> <NavLink  to={{pathname:"/publicFigure/"+d.firstName,state:d}} > read more </NavLink> </span></span>:
+                                 <span>{d.background.padEnd(d.background.length+5,'.')} <span> <NavLink  to={{pathname:"/publicFigure/"+d.firstName,state:d}} > read more </NavLink> </span></span>:
                                   <span>{d.background.substring(0,20).padEnd(25,'.')} <span><NavLink to={{pathname:"/publicFigure/"+d.firstName,state:d}} > read more </NavLink></span></span>}
                                 </span>}
                               </div>
@@ -146,31 +167,16 @@ axios.get("/v1/client/publicFigure/all/all/0").then(res=>{
                              <div className="user__field user__field3">{d.verified?<span className="user__verified"><i className="fa fa-check" aria-hidden="true"></i> verified</span>:null}</div>
                            </div>
 
-                       </div>
+
                     </div>
                   </div>
                   </>)}</div>
                  {this.state.loading?<button className="load__btn load__btn-loading">LOADING...</button>:null}
                  {this.state.totalPages==this.state.page+1 || this.state.loading?null: this.state.data.length<=0?<h1 style={{padding:"50px 50px"}}>No data</h1>:<button className="load__btn" onClick={this.pageHandler}>Load More...</button>}
-                 <div className="searchWrapper">
-                  <div className="notables__search">
-                      <input className="notables__search-input" type="text" value={this.state.search} onChange={(e)=>this.searchInputHandler(e)}  placeholder="search" name="" id=""/>
-                  </div>
-                  <div className="notables__searchByCountry">
-                  <input className="notables__searchByCountry-input" type="text"  onChange={(e)=>this.searchedCountriesHandler(e)} name="searchedCahrecterForCountry" placeholder="country search" value={this.state.searchedCahrecterForCountry} id=""/>
-                  <div className="notables__searchByCountry-dropdown">
-                  {this.state.searchedCountries.map(c=>
-                                       <div style={{zIndex:"1000"}} className="notables__searchByCountry-dropdown-item" onClick={()=> this.countrySelectHandler(c.substring(0,c.indexOf(',')))}>
-                                           <Flag code={c.substring(c.indexOf(',')+1)}height={16} />
-                                           {c.slice(0,c.length-3)}
-                                        </div>
-                                       )}
-                          <div className="notables__searchByCountry-dropdown-item" onClick={()=>this.countrySelectHandler("all")}>
-                              ALL
-                          </div>
-                      </div>
-                  </div>
-                  </div>
+
+
+
+
             </div></>
         )
     }
